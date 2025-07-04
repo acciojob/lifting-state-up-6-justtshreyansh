3881db50e81e58993c2ab3171d02f04111c5e530
= <=>
2025-07-04 23:08:36 +0530

    First commit of react

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import ToDoList from "./ToDoList";
+const arr = [{task:"Learn React",status:false},{task:"Build a React app",status:false},{task:"Deploy the React app",status:false},]
 const App = () => {
+  const[todos,setTodos] = useState(arr);
+  const handleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].status = true;  // mark complete
+    setTodos(newTodos);
+    console.log("clicked")
+  };
   return (
     <div>
+      <h1>Parent component</h1>
+      <ToDoList todos={todos} handleComplete={handleComplete}/>
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/components/ToDoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ToDoList = ({todos,handleComplete}) => {
+  return (
+    <div>
+      <h1>Child component</h1>
+      <ul>
+       {
+        todos.map((ele,index)=>{
+           return  <li key={index}><p>{ele.task}</p> {!ele.status && <button onClick={()=>handleComplete(index)}>Complete</button>}</li>
+        })
+       }
+      </ul>
+    </div>
+  )
+}
+
+export default ToDoList;
